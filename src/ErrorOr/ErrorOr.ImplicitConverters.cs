531ea46281fea31b4ee06ef82d72e70c3c6b2945
Kenny Pflug <kenny.pflug@live.de>
2024-05-09 19:34:29 +0200

    docs: add XML docs for exceptions
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/ErrorOr/ErrorOr.ImplicitConverters.cs
+++ src/ErrorOr/ErrorOr.ImplicitConverters.cs
@@ -21,6 +21,8 @@ public readonly partial record struct ErrorOr<TValue> : IErrorOr<TValue>
     /// <summary>
     /// Creates an <see cref="ErrorOr{TValue}"/> from a list of errors.
     /// </summary>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="errors"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when <paramref name="errors" /> is an empty list.</exception>
     public static implicit operator ErrorOr<TValue>(List<Error> errors)
     {
         if (errors is null)
@@ -39,6 +41,8 @@ public readonly partial record struct ErrorOr<TValue> : IErrorOr<TValue>
     /// <summary>
     /// Creates an <see cref="ErrorOr{TValue}"/> from a list of errors.
     /// </summary>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="errors"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when <paramref name="errors" /> is an empty array.</exception>
     public static implicit operator ErrorOr<TValue>(Error[] errors)
     {
         if (errors is null)
